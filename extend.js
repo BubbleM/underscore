2923ab690f6ffa3aaec628dba21948eff4059c4d
BubbleM <1870408473@qq.com>
2017-07-08 09:40:32 +0800

    standard note

--- extend.js
+++ extend.js
@@ -1,9 +1,10 @@
-/*
-  一个返回函数的函数，apply只是实现函数式编程的一种方法而已
+/**
+ * 一个返回函数的函数，apply只是实现函数式编程的一种方法而已
+ *
   splat()函数接受一个函数fun作为参数，并返回另一个函数
   返回的函数接受一个数组并用apply来执行函数fun
   这样，传入函数splat()的数组的元素是函数fun的参数
-*/
+ */
 function splat (fun) {
   return function (array) {
     return fun.apply(null, array)
@@ -12,9 +13,9 @@ function splat (fun) {
 var addArrayElement = splat(function (x, y) { return x + y })
 console.log(addArrayElement([1, 2])) // 3
 
-/*
-  unsplat()接受一个函数fun并返回另一个接受任意多个参数的函数，将参数转为数组传入函数fun并调用它
-*/
+/**
+ * unsplat()接受一个函数fun并返回另一个接受任意多个参数的函数，将参数转为数组传入函数fun并调用它
+ */
 function unsplat (fun) {
   return function () {
     return fun.call(null, _.toArray(arguments))
@@ -22,7 +23,9 @@ function unsplat (fun) {
 }
 var joinElements = unsplat(function (array) { return array.join(' ') })
 console.log(joinElements(1, 2)) // "1 2"
-/* 抽象方法 */
+/*
+ * 抽象方法
+ */
 function fail (thing) {
   throw new Error(thing)
 }
@@ -32,15 +35,15 @@ function warn (thing) {
 function note (thing) {
   console.log(['NOTE:', thing].join(' '))
 }
-/*
-  isIndexed()提供了判断某个数据是否是字符串或数组的函数抽象
-*/
+/**
+ * isIndexed()提供了判断某个数据是否是字符串或数组的函数抽象
+ */
 function isIndexed (data) {
   return _.isArray(data) || _.isString(data)
 }
-/*
-  nth()返回一个存储在允许索引访问的数据类型中的有效元素
-*/
+/**
+ * nth()返回一个存储在允许索引访问的数据类型中的有效元素
+ */
 function nativeNth (a, index) {
   if (!_.isNumber(index)) fail('Expected a number as the index')
   if (!isIndexed(a)) fail('Not supported on non-indexed type')
@@ -50,3 +53,7 @@ function nativeNth (a, index) {
 console.log(nativeNth('letters', 1)) // e
 console.log(nativeNth({}, 2)) // Error: Not supported on non-indexed type
 console.log(nativeNth('letters', 2000)) // Error: Index value is out of bounds
+/**
+ *
+ *
+ */
